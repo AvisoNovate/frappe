a47a49ccd070a3a76219bd45c028aae79b63fa58
Howard M. Lewis Ship <hlship@gmail.com>
2015-05-15 10:40:57 -0700

    Add codox documentation support

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
                            [com.stuartsierra/dependency "0.1.1"]]
             :profiles {:dev {
                              :dependencies [[io.aviso/pretty "0.1.18"]]
-                             }})
+                             }}
+            :codox {:src-dir-uri               "https://github.com/AvisoNovate/frappe/blob/master/"
+                    :src-linenum-anchor-prefix "L"
+                    :defaults                  {:doc/format :markdown}})
