65566486cf7fdc52f3aef92dbe2e73ad0e2df7c8
Howard M. Lewis Ship <hlship@gmail.com>
2015-05-14 17:04:01 -0700

    Make use of Stu's Dependency library

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject io.aviso/frappe "0.1.0"
-            :description "Functional/Reative programming"
+            :description "Functional/Reative Programming"
             :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                           [io.aviso/toolchest "0.1.2"]]
+                           [io.aviso/toolchest "0.1.2"]
+                           [com.stuartsierra/dependency "0.1.1"]]
             :profiles {:dev {
                              :dependencies [[io.aviso/pretty "0.1.18"]]
                              }})
