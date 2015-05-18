1a27c2893f25fc02f2907614bb0917951bc33f78
Howard M. Lewis Ship <hlship@gmail.com>
2015-05-18 10:53:50 -0700

    Convert to explicitly be about scanning file system (not classpath)

--- src/io/aviso/frappe/fs.clj
+++ src/io/aviso/frappe/fs.clj
@@ -20,25 +20,27 @@
         file-path (.getCanonicalPath file)]
     (subs file-path (inc (.length root-path)))))
 
-(defn- path->file
-  [path]
-  (-> path io/resource io/file))
-
-(defn find-resources
-  "Finds resources on the classpath starting from a root path. Files must pass though
+(defn find-file-cells
+  "Finds files within a root directory, starting from a root path. Files must pass though
   the file filter.
 
+  file-filter is a function that accepts a File instance and returns a truthy; only truthy
+  files will be included in the result (and directories, never).
+  The default file-filter includes everything.
+
   Returns a map of path (relative to the root-path) to file map cell."
-  [root-path file-cache file-filter]
-  (let [root-file (path->file root-path)
-        xf        (comp (remove #(= root-file %))
-                        (filter file-filter)
-                        (map #(let [path (relative-path root-file %)]
-                               [path (path->file-cell file-cache path %)])))]
-    (->>
-      root-file
-      file-seq
-      (into {} xf))))
+  ([root-path file-cache]
+   (find-file-cells root-path file-cache (constantly true)))
+  ([root-path file-cache file-filter]
+   (let [root-file (io/as-file root-path)
+         xf        (comp (remove #(.isDirectory %))
+                         (filter file-filter)
+                         (map #(let [path (relative-path root-file %)]
+                                [path (path->file-cell file-cache path %)])))]
+     (->>
+       root-file
+       file-seq
+       (into {} xf)))))
 
 
 
